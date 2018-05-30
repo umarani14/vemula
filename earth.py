4bd9f1d8bf52041db0d99a3706a9de47dbf55619
uma <vemulaumarani14@gmail.com>
2018-05-30 19:10:05 +0530

    second commit

--- earth.py
+++ earth.py
@@ -1,10 +1,8 @@
 # This program adds two numbers
 
-num1 = 1.5
-num2 = 6.3
-
+num1 = 1.6
+num2 = 6.4
 # Add two numbers
 sum = float(num1) + float(num2)
-
 # Display the sum
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
